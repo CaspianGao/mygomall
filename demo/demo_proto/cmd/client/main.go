d89d53aa87d765b00f8d965b85ecf379eb3328da
orlando <gao200418151@163.com>
2024-12-10 22:35:17 +0800

    04 coinfig

--- demo/demo_proto/cmd/client/main.go
+++ demo/demo_proto/cmd/client/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
-	"github.com/CaspianGao/mygomall/demo/demo_proto/kitex_gen/pbapi/echo"
+	"github.com/CaspianGao/mygomall/demo/demo_proto/kitex_gen/pbapi"
+	"github.com/CaspianGao/mygomall/demo/demo_proto/kitex_gen/pbapi/echoservice"
 	"github.com/cloudwego/kitex/client"
 	consul "github.com/kitex-contrib/registry-consul"
 )
@@ -12,10 +15,14 @@ func main() {
     r, err := consul.NewConsulResolver("127.0.0.1:8500")
     if err != nil {
         log.Fatal(err)
-    }
-    client, err := echo.NewClient("greet.server", client.WithResolver(r))
+    }  
+    c, err := echoservice.NewClient("demo_proto", client.WithResolver(r))
     if err != nil {
         log.Fatal(err)
     }
-    
+    res, err := c.Echo(context.TODO(), &pbapi.Request{Message: "Hello World!"})
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%v\n", res)
 }
